6ff500aee84faead6bbbdd3a13badb2bc3fb506e
ZAV2007 <zohairvadiwala@gmail.com>
2021-09-05 15:17:34 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDVV4zrsegV-JRC9-9a2Puf_QDBtiXANfI",
+  authDomain: "loginfinal-e8dd5.firebaseapp.com",
+  projectId: "loginfinal-e8dd5",
+  storageBucket: "loginfinal-e8dd5.appspot.com",
+  messagingSenderId: "630160966669",
+  appId: "1:630160966669:web:59d3e519449d2f17420144",
+  measurementId: "G-WV45JKN2B7",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
